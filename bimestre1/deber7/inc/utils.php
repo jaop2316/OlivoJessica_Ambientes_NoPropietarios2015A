4550ad2eb44e6b618f035998f7196030e5320bc8
Jessica Olivo <jessikita.ldu@gmail.com>
2015-11-24 14:49:15 -0500

    Este es el commit del deber7 actualizado

---
+++ bimestre1/deber7/inc/utils.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Redirige a la URL especificada
+ */
+function http_redirect( $url ){
+  header( 'Location: ' . DOCUMENT_ROOT_RELATIVA . $url );
+  exit;
+}
+
+function cerrar_sesion() {
+  $_SESSION = array();
+
+  // Si se desea destruir la sesión completamente, borre también la cookie de 
+  // sesión.
+  // Nota: ¡Esto destruirá la sesión, y no la información de la sesión!
+  if ( ini_get( "session.use_cookies" ) ) {
+      $params = session_get_cookie_params();
+      setcookie( session_name(), '', time() - 42000,
+          $params["path"], $params["domain"],
+          $params["secure"], $params["httponly"]
+      );
+  }
+  session_destroy();
+}
